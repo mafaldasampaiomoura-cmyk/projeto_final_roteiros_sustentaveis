f9d7740a854c3d459d520c237a3f07e27586c2d5
mafaldasampaiomoura-cmyk <mafaldasampaiomoura@gmail.com>
2026-03-26 11:34:41 +0000

    feat: add favourites feature

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router, Request, Response } from 'express';
 import authRoutes from './auth.routes';
 import routeRoutes from './routes.routes';
 import routePointsRoutes from './routePoints.routes';
+import favouritesRoutes from './favourites.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/routes', routeRoutes);
 router.use('/route-points', routePointsRoutes);
+router.use('/favourites', favouritesRoutes);
 
 router.get('/health', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'API is running' });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
-import app from './app';
+import app from "./app";
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`SERVER CERTO A CORRER EM http://localhost:${PORT}`);
 });
