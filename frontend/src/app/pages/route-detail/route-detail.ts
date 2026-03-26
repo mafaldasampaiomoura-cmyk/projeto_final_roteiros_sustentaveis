7a0d54b3ce194e21608bd6dd2545e45eaac2ae64
mafaldasampaiomoura-cmyk <mafaldasampaiomoura@gmail.com>
2026-03-26 21:57:30 +0000

    feat(frontend): connect route detail and display route points

--- frontend/src/app/pages/route-detail/route-detail.ts
+++ frontend/src/app/pages/route-detail/route-detail.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
+import { ActivatedRoute } from '@angular/router';
+import { RoutesService } from '../../services/routes';
 
 @Component({
   selector: 'app-route-detail',
@@ -7,4 +9,23 @@ import { Navbar } from '../../components/navbar/navbar';
   templateUrl: './route-detail.html',
   styleUrl: './route-detail.css',
 })
-export class RouteDetail {}
+export class RouteDetail implements OnInit {
+  routeId: string | null = null;
+  route: any = null;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private routesService: RoutesService
+  ) {}
+
+  ngOnInit(): void {
+    this.routeId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    if (this.routeId) {
+      this.routesService.getRouteById(this.routeId).subscribe((data: any) => {
+        console.log('ROTEIRO:', data);
+        this.route = data;
+      });
+    }
+  }
+}
